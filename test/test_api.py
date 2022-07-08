87df8936b4e34db547081d29464f562da4ee35af
reads <soldiddfouns@gmail.com>
2022-07-08 16:34:27 -0500

    Se agrega estructura básica de pruebas unitarias con sqlite

---
+++ test/test_api.py
@@ -0,0 +1,72 @@
+import random
+from fastapi.testclient import TestClient
+import sys
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+from main import app
+from app.db.models import Base
+
+from app.hashing import Hash
+from app.db.database import get_db
+
+
+db_path = os.path.join(os.path.dirname(__file__), "test.db")
+db_url = "sqlite:///{}".format(db_path)
+SQLALCHEMY_DATABASE_RUL = db_url
+engine_test = create_engine(
+    SQLALCHEMY_DATABASE_RUL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(bind=engine_test, autocommit=False, autoflush=False)
+Base.metadata.create_all(bind=engine_test)
+
+
+cliente = TestClient(app)
+
+
+# def insertar_usuario_prueba():
+#     password_hash = Hash.hash_password("prueba12")
+#     random_number = random.randint(1, 1000)
+#     engine_test.execute(
+#         f"""
+#         INSERT INTO usuario(username,password,nombre,apellido,direccion,telefono,correo)
+#         values
+#         ('prueba_{random_number}','{password_hash}','prueba_nombre','prueba_apellido','prueba_direccion',1212,'prueba_{random_number}@gmail.com')
+#         """
+#     )
+# insertar_usuario_prueba()
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+def test_crear_usuario():
+    usuario = {
+        "username": "dafma1",
+        "password": "Pruebas123*1",
+        "nombre": "Dafma1",
+        "apellido": "Reads1",
+        "direccion": "Toluca1",
+        "telefono": 1234561,
+        "correo": "dafma1@gmail.com",
+        "creacion": "2022-07-04T21:21:59.979195",
+    }
+    response = cliente.post('/user/', json=usuario)
+    assert response.status_code == 201
+    assert response.json()["respuesta"] == "Usuario creado correctamente!"
+
+
+# def test_delete_database():
+#     db_path = os.path.join(os.path.dirname(__file__), "test.db")
+#     os.remove(db_path)
